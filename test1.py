387fe978ac906f4caab6eda20ec4543b7e69f42b
Oscar Soler <osolertf@gmail.com>
2025-11-04 12:59:51 -0800

    Large update to repo, class working greatly. Image view and observation history to be improved

--- test1.py
+++ test1.py
@@ -1,13 +1,28 @@
-# test file
-import sys
+# Image reading test file
+
+import os, sys
 import numpy as np
 import matplotlib.pyplot as plt
+#%matplotlib qt
+from astropy.io import fits
 
-xx = np.arange(100)
-yy = np.sqrt(xx)
-yy2 = xx**2
-fig, ax = plt.subplots()
+wd = os.getcwd()
+directory = 'test_images'
+print('Working directory: '+ wd)
+print('Directory containing images: '+ directory)
+print('Available images:')
+dir_img = os.path.join(wd, directory)
+list_images = os.listdir(dir_img)
+print(list_images)
 
-ax.plot(xx, yy)
-ax.plot(xx, yy2)
-plt.show()
+image_file = list_images[0]
+image_data = fits.getdata(os.path.join(dir_img,image_file))
+
+print('Image data type: ', type(image_data))
+print('Image data shape: ', image_data.shape)
+
+plt.close(1)
+fig, ax = plt.subplots(num = 1)
+
+ax.imshow(image_data[:1000,:1000])
+plt.show() 
